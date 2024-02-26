e7a23dfdab51f182415fb7d45f79f47b4d5d5577
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-26 07:14:59 +0100

    add components

--- src/jsx/components/App.jsx
+++ src/jsx/components/App.jsx
@@ -1,16 +1,3 @@
-// import styles from './App.module.css';
-// import { DiApple } from 'react-icons/di';
-
-// export function App() {
-//   return (
-//     <div className={styles.container}>
-//       <h1>
-//         <DiApple /> Vite + React Template
-//       </h1>
-//     </div>
-//   );
-// }
-
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
